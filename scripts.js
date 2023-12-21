54347e605deaa0f933bac242d4fb3b443d66da9a
Lindemberg <lindembergparaiba@gmail.com>
2023-12-21 00:35:18 -0300

    funcao de concluir tarefas funcionando corretamente

--- scripts.js
+++ scripts.js
@@ -5,27 +5,34 @@ const taskList = document.querySelector(".task-list");
 let MyItemsList = [];
 
 function addNewTask() {
-  MyItemsList.push(input.value);
+  MyItemsList.push({
+    task: input.value,
+    completed: false,
+  });
   input.value = "";
   showTasks();
 }
 function showTasks() {
   let newLi = "";
 
-  MyItemsList.forEach((task, index) => {
+  MyItemsList.forEach((item, index) => {
     newLi =
       newLi +
       `
-    <li class="task">
-    <img src="img/checked.png" alt="imagem do check" />
-    <p>${task}</p>
-    <img src="img/trash.png" alt="imagem do check" onclick="deleteItem(${index})" />
-  </li>`;
+      <li class="task ${item.completed ? "done" : ""}">
+      <img src="img/checked.png" alt="imagem do check" onclick="completeTask(${index})" />
+      <p>${item.task}</p>
+      <img src="img/trash.png" alt="imagem do check" onclick="deleteItem(${index})" />
+    </li>`;
   });
   taskList.innerHTML = newLi;
 }
 function deleteItem(index) {
   MyItemsList.splice(index, 1);
+  showTasks();
+}
+function completeTask(index) {
+  MyItemsList[index].completed = !MyItemsList[index].completed;
   showTasks();
 }
 
